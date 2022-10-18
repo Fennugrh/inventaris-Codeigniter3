cbb6602b0853b64c035be4e1b34f0ab8c18b31b4
Fennugrh <anviewyosh@gmail.com>
2022-10-19 00:35:35 +0700

    edited

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -5,7 +5,12 @@ class User extends CI_Controller
 {
     public function index()
     {
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
         $data['title'] = 'Dashboard';
+        $this->load->view('layouts/header', $data);
+        $this->load->view('layouts/navbar', $data);
+        $this->load->view('layouts/sidebar', $data);
         $this->load->view('user/index', $data);
+        $this->load->view('layouts/footer');
     }
 }
